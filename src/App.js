94818811bb295a860485d22f9680e61ed337ba34
Razin Kabir <kabirrazin@gmail.com>
2024-05-07 07:14:18 -0400

    stripe react UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Stripe from "react-stripe-checkout";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function handleToken(token) {
+console.log(token);
+await axios.post("http://localhost:8080/api/payment/charge", "", {         headers: {
+  token: token.id,
+  amount: 500,
+},}).then(() => {
+   alert("Payment Success");
+   }).catch((error) => {
+   alert(error);
+   });
 }
-
+return (
+<div className="App">
+<Stripe
+stripeKey="pk_test_51P1yv3HAYSgpUy4mjpoYvnjysY2o9np3BIDw1BC1XFNYcWgLQ7ryuQb5KWLBGf3WRVpfmIhq8NG7msJ37dfXs7Ev00qRCOBtTf"
+token={handleToken}
+/>
+</div>
+);
+}
 export default App;
